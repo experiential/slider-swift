48e2bd4459615a1a4a8b943fe21fa7158ef85229
James Sanford <jamesthaimassage@gmail.com>
2018-12-14 22:23:25 +0000

    Initial Commit

---
+++ Slider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Slider
+//
+//  Created by James Sanford on 14/12/2018.
+//  Copyright © 2018 Experiential Services. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
